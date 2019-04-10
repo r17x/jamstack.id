bc0396ed70987e52277000eff58eb3cee87c2375
ri7nz <ri7nz.labs@gmail.com>
2019-04-11 04:27:53 +0700

    Change Anchorlink scroll function

--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,76 @@
 // mobile menu
 
-$(function(){
-  $('#menu').slicknav({
-    brand: '<a class="mobile-logo" href="/"><img src="/img/jamstack-full-logo.svg"/></a>',
-    label: '',
-    appendTo: 'header'
+$(function() {
+  $("#menu").slicknav({
+    brand:
+      '<a class="mobile-logo" href="/"><img src="/img/jamstack-full-logo.svg"/></a>',
+    label: "",
+    appendTo: "header"
   });
 });
-
 
 // Sticky Nav Functionality in Vanilla JS
 
 function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className)
+  if (el.classList) return el.classList.contains(className);
   else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+    return !!el.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
 }
 
 function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className)
-  else if (!hasClass(el, className)) el.className += " " + className
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += " " + className;
 }
 
 function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className)
+  if (el.classList) el.classList.remove(className);
   else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    el.className=el.className.replace(reg, ' ')
+    const reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+    el.className = el.className.replace(reg, " ");
   }
 }
 
-var header = document.getElementById("header");
+const header = document.getElementById("header");
 
 window.onscroll = function() {
-
-  var currentWindowPos = document.documentElement.scrollTop || document.body.scrollTop;
+  const currentWindowPos =
+    document.documentElement.scrollTop || document.body.scrollTop;
 
   if (currentWindowPos > 0) {
-    addClass(header, 'scrolled');
+    addClass(header, "scrolled");
   } else {
-    removeClass(header, 'scrolled');
+    removeClass(header, "scrolled");
   }
 };
 
-
 // scrolling for anchor links
+window.onload = function() {
+  const addAnchorClick = function(evt) {
+    evt.preventDefault();
+    const getHref = evt.currentTarget.getAttribute("href");
+    const currentHref = document.getElementById(getHref.replace("#", ""));
+    anchorScroll(evt.currentTarget, currentHref, 1000);
+  };
 
-$(document).ready(function(){
-  $(".scrolling-link").click(function(e) {
-    e.preventDefault();
+  const eachScrollElement = function(scroll) {
+    if (scroll && !scroll.onclick) {
+      scroll.onclick = addAnchorClick;
+    }
+  };
 
-    anchorScroll( $(this), $($(this).attr("href")), 1000 );
-  });
-});
+  Array.from(document.getElementsByClassName("scrolling-link")).forEach(
+    eachScrollElement
+  );
+};
 
 function anchorScroll(this_obj, that_obj, base_speed) {
-  var this_offset = this_obj.offset();
-  var that_offset = that_obj.offset();
-  var offset_diff = Math.abs(that_offset.top - this_offset.top);
-  var speed       = 1000;
+  const offset_diff = Math.abs(this_obj.offsetTop - that_obj.offsetTop);
+  const speed = 1000;
 
-  $("html,body").animate({
-    scrollTop: Math.abs(that_offset.top - 125)
-  }, speed);
+  $("html,body").animate(
+    {
+      scrollTop: Math.abs(that_obj.offsetTop - 125)
+    },
+    speed
+  );
 }
